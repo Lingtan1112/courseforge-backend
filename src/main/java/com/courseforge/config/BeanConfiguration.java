89087bacf02e99bfb45ad5f762ccb85247962df0
Lingtan1112 <77401106+Lingtan1112@users.noreply.github.com>
2023-11-11 00:39:16 +0530

    Update BeanConfiguration.java

--- src/main/java/com/courseforge/config/BeanConfiguration.java
+++ src/main/java/com/courseforge/config/BeanConfiguration.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
